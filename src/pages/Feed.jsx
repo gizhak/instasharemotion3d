ec2846d3585f2035e4480b9def700ac5cf4d9948
Lea Guzmicka <lea.guzmicka@outlook.com>
2026-01-08 21:09:47 +0200

    fixing grid in postList and Start work on Feed

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,39 +1,40 @@
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
+import { loadPosts } from '../store/actions/post.actions';
+import { SvgIcon } from '../cmps/SvgIcon';
+
 export function Feed() {
+	const posts = useSelector((storeState) => storeState.postModule.posts);
+
+	useEffect(() => {
+		loadPosts();
+	}, []);
+
+	console.log('posts:', posts);
+
 	return (
 		<section className="home">
 			Feed
 			<section className="feed-grid-container">
-				<article>
-					<h2>Hello there</h2>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-					<img src="/img/sunflowers.jpg" />
-				</article>
-				<article>
-					<h2>Hello there</h2>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio,
-						ea quis quae odio dolores excepturi quod ratione
-					</p>
-					<img src="/img/sunflowers.jpg" />
-				</article>
-				<article>
-					<h2>Hello there</h2>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio,
-						ea quis quae odio dolores excepturi
-					</p>
-					<img src="/img/sunflowers.jpg" />
-				</article>
-				<article>
-					<h2>Hello there</h2>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio,
-						ea quis quae odio dolores excepturi quod ratione, cumque voluptatem
-						repudiandae temporibus dolor modi. Corporis debitis molestias at eos
-						corrupti!
-					</p>
-					<img src="/img/sunflowers.jpg" />
-				</article>
+				{posts &&
+					posts.map((post) => (
+						<article key={post._id} className="post-article flex row">
+							<div className="post-header">
+								<img className="post-profile-img" src={post.by.imgUrl}></img>
+								<h4>{post.by.fullname}</h4>
+								<SvgIcon iconName="postDots" />
+							</div>
+							<img className="post-img" src={post.imgUrl}></img>
+							<div className="post-desc">
+								<h4>
+									{post.by.fullname}: <span>{post.txt}</span>
+								</h4>
+								<SvgIcon iconName="like" />
+								<SvgIcon iconName="comment" />
+							</div>
+						</article>
+					))}
 			</section>
 		</section>
 	);
