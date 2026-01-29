4c7d49dcb89a591de1619e485b49d7ec443b00c5
User Guy <decoding.c.t@gmail.com>
2026-01-29 22:47:25 +0200

    Optimize build with code splitting

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,21 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	// After setting-up a backend, this can automoate the copying process of the built files:
-	// build: {
-	// 	outDir: '../backend/public',
-	// 	emptyOutDir: true,
-	// },
+	build: {
+		// outDir: '../backend/public',
+		// emptyOutDir: true,
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					// Split vendor code
+					'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+					'three-vendor': ['three', '@react-three/fiber', '@react-three/drei', '@react-three/postprocessing'],
+					'mediapipe': ['@mediapipe/tasks-vision']
+				}
+			}
+		},
+		chunkSizeWarningLimit: 1000
+	},
 	// If we want to build a local version (that uses local services)
 	// define: {
 	// 	'process.env.VITE_LOCAL': 'true'
